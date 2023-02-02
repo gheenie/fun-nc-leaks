bdecba5944a909d85e8ae9823e9897696202260c
infoomics <sbx_94@hotmail.com>
2023-02-02 14:42:10 +0000

    added code for getInterests()

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ const https = require('https');
 const fs = require('fs');
 
 function boop(){
-const options = {
-    hostname: 'nc-leaks.herokuapp.com',
-    path: '/api/confidential',
-    method: 'GET'
-};
+    const options = {
+        hostname: 'nc-leaks.herokuapp.com',
+        path: '/api/confidential',
+        method: 'GET'
+    };
 
-const req = https.request(options, (response) => {
-    let body = '';
+    const req = https.request(options, (response) => {
+        let body = '';
 
-    response.on('data', (packet) => {
-        body += packet.toString();
+        response.on('data', (packet) => {
+            body += packet.toString();
+        });
+
+        response.on('end', () => {
+            const parsedBody = JSON.parse(body);
+            //console.log(parsedBody);
+
+            const instructions = parsedBody.instructions;
+            //console.log(instructions);
+            fs.writeFile(`instructions.md`, instructions, () => {});
+        });
     });
 
-    response.on('end', () => {
-        const parsedBody = JSON.parse(body);
-        //console.log(parsedBody);
-
-        const instructions = parsedBody.instructions;
-        //console.log(instructions);
-        fs.writeFile(`instructions.md`, instructions, () => {});
-    });
-});
-
-req.end();}
+    req.end();
+}
 
 // // Write a function called `getPeople` that will retrieve list of all the available people on the `northcoders` server . This should:
 // 1. Use node's `https` module to make a request to `https://nc-leaks.herokuapp.com/api/people`.
@@ -34,36 +35,84 @@ req.end();}
 
 function getPeople() {
     const options = {
-    hostname: 'nc-leaks.herokuapp.com',
-    path: '/api/people',
-    method: 'GET'
-};
+        hostname: 'nc-leaks.herokuapp.com',
+        path: '/api/people',
+        method: 'GET'
+    };
 
-const req = https.request(options, (response) => {
-    let body = '';
+    const req = https.request(options, (response) => {
+        let body = '';
 
-    response.on('data', (packet) => {
-        body += packet.toString();
+        response.on('data', (packet) => {
+            body += packet.toString();
+        });
+
+        response.on('end', () => {
+            const parsedBody = JSON.parse(body);
+            const obtainObject = parsedBody.people;
+            const northcoderPeople = obtainObject.filter(person => person.job.workplace === "northcoders");
+            const northcoderPeopleInJson = { people: northcoderPeople };
+
+            fs.writeFile(`northcoder.json`, JSON.stringify(northcoderPeopleInJson), () => {});
+        });
     });
 
-    response.on('end', () => {
-        const parsedBody = JSON.parse(body);
-        const obtainObject = parsedBody.people
-        const northcoderPeople = obtainObject.filter(person => person.job.workplace === "northcoders")
-        // console.log(northcoderPeople);
-
-        let Str = ""
-        northcoderPeople.forEach(element => { Str += JSON.stringify(element)
-        });;
-        
-        console.log(Str);
-        fs.writeFile(`northcoder.json`, Str, () => {});
-    });
-});
-
-req.end()
-
-
+    req.end()
 }
 
-getPeople()
+//getPeople();
+
+/*Write a function called getInterests that uses the newly found usernames for each northcoder to retrieve information on everyone's interests. This function should:
+
+    Use fs to read the northcoders.json file you created in task 1
+    
+    For every person, use their username and make a request to https://nc-leaks.herokuapp.com/api/people/:username/interests to get their interests.
+    Each response will be an object with a person key. Collect up the data at this person key into an array.
+    Once you have all responses in the array, save it to a file called interests.json.
+*/
+
+function getInterests() {
+    fs.readFile('northcoder.json', (err, data) => {
+        if (err) throw err;
+        else {
+            const northcoderPeopleInJson = JSON.parse(data);
+            const personAndInterests = [];
+            let count = 0;
+
+            northcoderPeopleInJson.people.forEach((northcoder, i) => {
+                const username = northcoder.username;
+                const options = {
+                    hostname: 'nc-leaks.herokuapp.com',
+                    path: `/api/people/${username}/interests`,
+                    method: 'GET'
+                };
+
+                const req = https.request(options, (response) => {
+                    let body = '';
+            
+                    response.on('data', (packet) => {
+                        body += packet.toString();
+                    });
+            
+                    response.on('end', () => {
+                        const parsedBody = JSON.parse(body);
+                        
+                        personAndInterests[i] = parsedBody.person;
+
+                        count++;
+                        
+                        if (count === northcoderPeopleInJson.people.length) {
+                            const personAndInterestsInJson = { personAndInterests: personAndInterests};
+
+                            fs.writeFile(`interests.json`, JSON.stringify(personAndInterestsInJson), () => {});
+                        }
+                    });
+                });
+            
+                req.end()
+            });
+        }
+    });
+}
+
+getInterests();
