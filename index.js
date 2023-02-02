c26a7a1024c70f3dd66d1dbba696affb1866e393
infoomics <sbx_94@hotmail.com>
2023-02-02 17:48:32 +0000

    removed task comments; changed func names for refactored ones

--- index.js
+++ index.js
@@ -29,10 +29,6 @@ function boop() {
     req.end();
 }
 
-// // Write a function called `getPeople` that will retrieve list of all the available people on the `northcoders` server . This should:
-// 1. Use node's `https` module to make a request to `https://nc-leaks.herokuapp.com/api/people`.
-// 2. Once you have the response as a useable object, look through the people to find anyone who has `northcoders` as the workplace.
-// 3. Save these `northcoders` employees to a file called `northcoders.json` - remember that the data argument of `fs.writeFile` must be of type string\* so you may need to manipulate the data before saving it.
 
 function getPeople() {
     const options = {
@@ -65,17 +61,7 @@ function getPeople() {
 
 //getPeople();
 
-/*Write a function called getInterests that uses the newly found usernames for each northcoder to retrieve information on everyone's interests. This function should:
-
-    Use fs to read the northcoders.json file you created in task 1
-    
-    For every person, use their username and make a request to https://nc-leaks.herokuapp.com/api/people/:username/interests to get their interests.
-    Each response will be an object with a person key. Collect up the data at this person key into an array.
-    Once you have all responses in the array, save it to a file called interests.json.
-*/
-
-// Not using superagent.
-/*function getInterests() {
+function getInterests() {
     fs.readFile('northcoder.json', (err, data) => {
         if (err) throw err;
         else {
@@ -119,9 +105,11 @@ function getPeople() {
             });
         }
     });
-}*/
+}
 
-function getInterests() {
+//getInterests();
+
+function getInterestsUsingSuperagent() {
     fs.readFile('northcoder.json', (err, data) => {
         if (err) throw err;
         else {
@@ -153,15 +141,54 @@ function getInterests() {
     });
 }
 
-//getInterests();
-
-// ### Task 3
-
-// Write a function called `getPets` that does the same as the Task 2 but for pets. The endpoint is `https://nc-leaks.herokuapp.com/api/people/:username/pets`;
-
-// > Note: Some of the users do not have pets and so the server will respond with a person but an empty pets array! These responses should not be included in the `pets.json`.
+//getInterestsUsingSuperagent();     
 
 function getPets() {
+    fs.readFile('northcoder.json', (err, data) => {
+        if (err) throw err;
+        else {
+            const northcoderPeopleInJson = JSON.parse(data);
+            const personAndPets = []
+            let count = 0
+
+            northcoderPeopleInJson.people.forEach((northcoder, i) => {
+                const username = northcoder.username;
+                const options = {
+                    hostname: 'nc-leaks.herokuapp.com',
+                    path: `/api/people/${username}/pets`,
+                    method: 'GET'
+                };
+
+                const req = https.request(options, (response) => {
+                    let body = '';
+            
+                    response.on('data', (packet) => {
+                        body += packet.toString();
+                    });
+            
+                    response.on('end', () => {
+                        const parsedBody = JSON.parse(body);
+                        personAndPets[i] = parsedBody.person;
+                        count++;
+                        
+                        if (count === northcoderPeopleInJson.people.length) {
+                            const filteredPersonAndPets = personAndPets.filter(element => element !== undefined)
+                            const personAndPetsInJson = { personAndPets: filteredPersonAndPets};
+                            console.log(filteredPersonAndPets);
+                            fs.writeFile(`pets.json`, JSON.stringify(personAndPetsInJson), () => {});
+                        }
+                    });
+                });
+                req.end()
+            });
+        }
+
+    });
+}
+
+//getPets()
+
+function getPetsUsingSuperagent() {
     fs.readFile('northcoder.json', (err, data) => {
         if (err) throw err;
         else {
@@ -189,65 +216,15 @@ function getPets() {
                             const filteredPersonAndPets = personAndPets.filter(element => element !== undefined)
                             const personAndPetsInJson = { personAndPets: filteredPersonAndPets};
                             fs.writeFile(`petss.json`, JSON.stringify(personAndPetsInJson), () => {});  
-                                        }
+                        }
                     }
                 })
             })
         }    
     })
-}       
+}
 
-getPets()
-
-//getPets()
-
-// function getPets() {
-//     fs.readFile('northcoder.json', (err, data) => {
-//         if (err) throw err;
-//         else {
-//             const northcoderPeopleInJson = JSON.parse(data);
-//             const personAndPets = []
-//             let count = 0
-
-//             northcoderPeopleInJson.people.forEach((northcoder, i) => {
-//                 const username = northcoder.username;
-//                 const options = {
-//                     hostname: 'nc-leaks.herokuapp.com',
-//                     path: `/api/people/${username}/pets`,
-//                     method: 'GET'
-//                 };
-
-//                 const req = https.request(options, (response) => {
-//                     let body = '';
-            
-//                     response.on('data', (packet) => {
-//                         body += packet.toString();
-//                     });
-            
-//                     response.on('end', () => {
-//                         const parsedBody = JSON.parse(body);
-//                         personAndPets[i] = parsedBody.person;
-//                         count++;
-                        
-//                         if (count === northcoderPeopleInJson.people.length) {
-//                             const filteredPersonAndPets = personAndPets.filter(element => element !== undefined)
-//                             const personAndPetsInJson = { personAndPets: filteredPersonAndPets};
-//                             console.log(filteredPersonAndPets);
-//                             fs.writeFile(`pets.json`, JSON.stringify(personAndPetsInJson), () => {});
-//                         }
-//                     });
-//                 });
-//                 req.end()
-//             });
-//         }
-
-//     });
-// }
-
-/*Automation is great. Create a function called scavengeForNcData that uses all of the functions you created in Tasks 1-3 to automate your hunt for data.
-
-    Note: Remember getInterests and getPets must only be used when you can be sure that the northcoders.json has finished being created. Considering these are all asynchronous functions, how can you ensure this?
-*/
+//getPetsUsingSuperagent();
 
 function scavengeForNcData() {
     getPeople();
